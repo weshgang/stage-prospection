36edb443ecba287912fdd9db0b172d59c0aba3c8
weshgang <mael.mispelblom@gmail.com>
2025-07-17 22:05:44 +0200

    envvar

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const url = import.meta.env.VITE_SUPABASE_URL;
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
-export const supabase = createClient(url, key);
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
